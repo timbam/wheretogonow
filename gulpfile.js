df4864547da0a5479b7eb5e0a264c881e6a74192
timbam <timbarbier88@gmail.com>
2019-05-04 17:55:14 +0200

    Added gulp.series for gulp to work in version 4

--- gulpfile.js
+++ gulpfile.js
@@ -28,70 +28,79 @@ var dependencies = [
  | Compile third-party dependencies separately for faster performance.
  |--------------------------------------------------------------------------
  */
-gulp.task("browserify-vendor", function() {
-  return browserify()
-    .require(dependencies)
-    .bundle()
-    .pipe(source("vendor.bundle.js"))
-    .pipe(buffer())
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(gulp.dest("public/js"));
-});
+gulp.task(
+  "browserify-vendor",
+  gulp.series(function() {
+    return browserify()
+      .require(dependencies)
+      .bundle()
+      .pipe(source("vendor.bundle.js"))
+      .pipe(buffer())
+      .pipe(gulpif(production, uglify({ mangle: false })))
+      .pipe(gulp.dest("public/js"));
+  })
+);
 
 /*
  |--------------------------------------------------------------------------
  | Compile only project files, excluding all third-party dependencies.
  |--------------------------------------------------------------------------
  */
-gulp.task("browserify", ["browserify-vendor"], function() {
-  return browserify({ entries: "src/index.js", debug: true })
-    .external(dependencies)
-    .transform(babelify, { presets: ["react", "es2015", "stage-1"] })
-    .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("public/js"));
-});
+gulp.task(
+  "browserify",
+  gulp.series("browserify-vendor", function() {
+    return browserify({ entries: "src/index.js", debug: true })
+      .external(dependencies)
+      .transform(babelify, { presets: ["react", "es2015", "stage-1"] })
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(gulpif(production, uglify({ mangle: false })))
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest("public/js"));
+  })
+);
 
 /*
  |--------------------------------------------------------------------------
  | Same as browserify task, but will also watch for changes and re-compile.
  |--------------------------------------------------------------------------
  */
-gulp.task("browserify-watch", ["browserify-vendor"], function() {
-  var bundler = watchify(
-    browserify({ entries: "src/index.js", debug: true }, watchify.args)
-  );
-  bundler.external(dependencies);
-  bundler.transform(babelify, { presets: ["react", "es2015", "stage-1"] });
-  bundler.on("update", rebundle);
-  return rebundle();
+gulp.task(
+  "browserify-watch",
+  gulp.series("browserify-vendor", function() {
+    var bundler = watchify(
+      browserify({ entries: "src/index.js", debug: true }, watchify.args)
+    );
+    bundler.external(dependencies);
+    bundler.transform(babelify, { presets: ["react", "es2015", "stage-1"] });
+    bundler.on("update", rebundle);
+    return rebundle();
 
-  function rebundle() {
-    var start = Date.now();
-    return bundler
-      .bundle()
-      .on("error", function(err) {
-        gutil.log(gutil.colors.red(err.toString()));
-      })
-      .on("end", function() {
-        gutil.log(
-          gutil.colors.green(
-            "Finished rebundling in",
-            Date.now() - start + "ms."
-          )
-        );
-      })
-      .pipe(source("bundle.js"))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write("."))
-      .pipe(gulp.dest("public/js/"));
-  }
-});
+    function rebundle() {
+      var start = Date.now();
+      return bundler
+        .bundle()
+        .on("error", function(err) {
+          gutil.log(gutil.colors.red(err.toString()));
+        })
+        .on("end", function() {
+          gutil.log(
+            gutil.colors.green(
+              "Finished rebundling in",
+              Date.now() - start + "ms."
+            )
+          );
+        })
+        .pipe(source("bundle.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("public/js/"));
+    }
+  })
+);
 
 /*
  |--------------------------------------------------------------------------
