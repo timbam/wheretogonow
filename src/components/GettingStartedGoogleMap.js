40a1c7a4210b42c992a75d2226266ae4c0695b01
timbam <timbarbier88@gmail.com>
2019-05-04 16:50:14 +0200

    added node version in package.json

--- src/components/GettingStartedGoogleMap.js
+++ src/components/GettingStartedGoogleMap.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { withGoogleMap, GoogleMap, Marker, Circle } from 'react-google-maps';
-import { SYMBOLS } from '../database/constants';
+import React from "react";
+import { withGoogleMap, GoogleMap, Marker, Circle } from "react-google-maps";
+import { SYMBOLS } from "../database/constants";
 
 const GettingStartedGoogleMap = withGoogleMap(props => {
   // console.log(props.weather);
   let centerChangedTimeout, singleClickTimeout;
-  const {mapCenter, epicenter} = props.weather;
-  const {nearbyCitiesWeather} = props.weather;
+  const { mapCenter, epicenter } = props.weather;
+  const { nearbyCitiesWeather } = props.weather;
 
   function handleDblClick(e) {
     clearTimeout(singleClickTimeout);
@@ -21,62 +21,79 @@ const GettingStartedGoogleMap = withGoogleMap(props => {
     }, 300);
   }
 
-  function onCenterChanged(){
-    if(centerChangedTimeout){
+  function onCenterChanged() {
+    if (centerChangedTimeout) {
       clearTimeout(centerChangedTimeout);
       centerChangedTimeout = null;
     }
     centerChangedTimeout = setTimeout(props.onCenterChanged, 100);
   }
 
-  function EpicenterRender(){
-    if(epicenter){
+  function EpicenterRender() {
+    if (epicenter) {
       return (
         <div>
           <Circle
-            center={{lat: epicenter.coordinates[0], lng: epicenter.coordinates[1] }}
-            radius={props.maps.circleRadius*1000} //Convert km to m
+            center={{
+              lat: epicenter.coordinates[0],
+              lng: epicenter.coordinates[1]
+            }}
+            radius={props.maps.circleRadius * 1000} //Convert km to m
             visible={props.maps.circleVisible}
             options={{
               fillColor: "blue",
-              strokeColor: '#269abc',
+              strokeColor: "#269abc",
               strokeOpacity: 0.75,
               fillOpacity: 0
             }}
           />
         </div>
       );
-    } return null;
+    }
+    return null;
   }
   return (
-        <GoogleMap
-        ref={props.onMapLoad}
-        onDblClick={handleDblClick}
-        onClick={handleSingleClick}
-        onCenterChanged={onCenterChanged}
-        defaultZoom={6}
-        options= {{
-          disableDoubleClickZoom: true,
-          mapTypeControl: false,
-          streetViewControl: false
-        }}
-        center={{lat: mapCenter[0], lng: mapCenter[1] }}
-        >
-          {nearbyCitiesWeather.map((city, index) => {
-            var {maxTemperature} = city.weatherArray[props.weather.dayIndex];
-            return(
-              <Marker
-                onClick={() => {props.onMarkerClick(city._id)}}
-                key={index}
-                position={{lat: parseFloat(city.weatherArray[0].latitude), lng: parseFloat(city.weatherArray[0].longitude)}}
-                defaultAnimation={2}
-                icon={"/sym/b48/" + SYMBOLS[Number(city.weatherArray[props.weather.dayIndex].symbol.number)-1] + ".png"}
-                label={(index + 1).toString()}
-              />
-            );
-          })}
-          <EpicenterRender />
-        </GoogleMap>
+    <GoogleMap
+      ref={props.onMapLoad}
+      onDblClick={handleDblClick}
+      onClick={handleSingleClick}
+      onCenterChanged={onCenterChanged}
+      defaultZoom={6}
+      options={{
+        disableDoubleClickZoom: true,
+        mapTypeControl: false,
+        streetViewControl: false
+      }}
+      center={{ lat: mapCenter[0], lng: mapCenter[1] }}
+    >
+      {nearbyCitiesWeather.map((city, index) => {
+        var { maxTemperature } = city.weatherArray[props.weather.dayIndex];
+        return (
+          <Marker
+            onClick={() => {
+              props.onMarkerClick(city._id);
+            }}
+            key={index}
+            position={{
+              lat: parseFloat(city.weatherArray[0].latitude),
+              lng: parseFloat(city.weatherArray[0].longitude)
+            }}
+            defaultAnimation={2}
+            icon={
+              "/sym/b48/" +
+              SYMBOLS[
+                Number(
+                  city.weatherArray[props.weather.dayIndex].symbol.number
+                ) - 1
+              ] +
+              ".png"
+            }
+            label={(index + 1).toString()}
+          />
+        );
+      })}
+      <EpicenterRender />
+    </GoogleMap>
   );
 });
 export default GettingStartedGoogleMap;
